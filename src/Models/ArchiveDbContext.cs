5b9e7f762ebc1d6d0098285470ee90ffd1e9e0e7
Christopher Ravailhe <19819051+Chrisdbhr@users.noreply.github.com>
2026-05-21 20:59:54 -0300

    feat: add ArchiveDbContext

---
+++ src/Models/ArchiveDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Models;
+
+public class ArchiveDbContext : DbContext
+{
+    public DbSet<MessageRecord> Messages => Set<MessageRecord>();
+
+    public ArchiveDbContext() { }
+
+    public ArchiveDbContext(DbContextOptions<ArchiveDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MessageRecord>(entity =>
+        {
+            entity.ToTable("Messages");
+
+            entity.HasIndex(e => e.AuthorId);
+            entity.HasIndex(e => e.ChannelId);
+            entity.HasIndex(e => e.GuildId);
+            entity.HasIndex(e => e.MessageId).IsUnique();
+        });
+    }
+}
